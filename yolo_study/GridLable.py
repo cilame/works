9d7cf1aba7b97bfca77218fbcca30b065ce27fa3
cilame <2473495041@qq.com>
2017-12-31 16:04:09 +0800

    add

--- yolo_study/GridLable.py
+++ yolo_study/GridLable.py
@@ -2,15 +2,22 @@ import numpy as np
 
 class GridLable:
     def __init__(self,*args,**kw):
+        '''
+        **kw:
+            twth:   a tuple(tw, th)
+                true width and true height
+            gridwh: a tuple(gridw, gridh)
+                grid
+        '''
         self._tw,self._th = map(float,kw.get('twth'))
         self._gridw,self._gridh = map(float,kw.get('gridwh'))
         self._gridkw = self._tw / self._gridw
         self._gridkh = self._th / self._gridh
-        self._create_grid()
         
     def _create_grid(self):
         y, x = np.mgrid[:self._gridh,:self._gridw]
-        self.indx = np.concatenate((y[...,None],x[...,None]),axis=-1)
+        indx = np.concatenate((y[...,None],x[...,None]),axis=-1)
+        return indx
         
     def _get_index(self,x,y):
         sh = np.arange(0,self._th,self._th/self._gridh)
@@ -19,12 +26,32 @@ class GridLable:
         w = max(np.where(sw<x)[0])
         return h,w
 
-    def create_label(self, xywh):
+    def _test_create_label(self, xywh):
         x,y,w,h = map(float, xywh)
         locy,locx = self._get_index(x,y)
         sigx = x / self._gridkw - locx
         sigy = y / self._gridkh - locy
         return 'scah,scaw,sigx,sigy:',locx,locy,sigx,sigy
 
-s = GridLable(twth = (90,90), gridwh = (3,3))
-print s.create_label(xywh = (50,40,3,3))
+    def _create_sig(self, xywh):
+        x,y,w,h = map(float, xywh)
+        locy,locx = self._get_index(x,y)
+        sigx = x / self._gridkw - locx
+        sigy = y / self._gridkh - locy
+        return locx,locy,sigx,sigy
+
+    # test other zero
+    def create_label(self, xywh):
+        locx,locy,sigx,sigy = self._create_sig(xywh)
+        indx = self._create_grid()
+        new_indx = indx + [sigy,sigx]
+        mask = np.zeros(new_indx.shape)
+        mask[locy,locx] = new_indx[locy,locx]
+        return mask,locy,locx
+
+        
+if __name__ == "__main__":
+    # cur test
+    s = GridLable(twth = (90,90), gridwh = (3,3))
+    print s.create_label((15, 59, 30, 30))#yxhw
+    
--- yolo_study/create_sample.py
+++ yolo_study/create_sample.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import cv2
 import numpy as np
 import random as rd
@@ -9,6 +10,40 @@ rd_outwin = False
 w,h = 90,90
 gridw,gridh = 3,3
 
+
+
+f = open('pic.txt','a')
+if not os.path.isdir('pic'):
+    os.mkdir('pic')
+
+
+
+
+###################内部为数据转换函数，用以对边框进行限制
+def to_xyxy(x,y,w,h):
+    x1 = x - w/2
+    x2 = x + w/2
+    y1 = y - h/2
+    y2 = y + h/2
+    return x1,y1,x2,y2
+
+def to_xywh(x1,y1,x2,y2):
+    x = (x1 + x2)/2
+    y = (y1 + y2)/2
+    w = x2 - x1
+    h = y2 - y1
+    return x, y, w, h
+
+def local_size(x1,y1,x2,y2,maxx,maxy):
+    if x1 < 0:x1 = 0
+    if y1 < 0:y1 = 0
+    if x2 > maxx:x2 = maxx
+    if y2 > maxy:y2 = maxy
+    return x1,y1,x2,y2
+###################
+
+
+# 根据总长度和 grid 获取一个 grid 区域以及随机偏差为 .25 的坐标
 def get_anchors(lens,grid):
     anchors = {}
     for i in range(grid):
@@ -16,10 +51,11 @@ def get_anchors(lens,grid):
         anchors[i] = [i, int((i+0.25) * anchor),int((i+0.75) * anchor)]
     return anchors
 
+# 根据总长度和 grid 获取一个简单的随机边框大小
 def get_rdscal(lens,grid):
-    return int((rd.random()*.7+.7)*lens/grid)
-    
+    return int((rd.random()*.8+.9)*lens/grid)
 
+# 就是前两个函数的综合利用，通过长宽以及长宽上的 grid 获取相应的随机边框及其坐标框
 def get_rdxywh(w,h,gridw,gridh):
     anchorsy = get_anchors(h,gridh)
     anchorsx = get_anchors(w,gridw)
@@ -33,36 +69,19 @@ def get_rdxywh(w,h,gridw,gridh):
     rdscaly = rdareay[0]
     return rdx,rdy,rdw,rdh,rdscalx,rdscaly
 
-def to_xyxy(x,y,w,h):
-    x1 = x - w/2
-    x2 = x + w/2
-    y1 = y - h/2
-    y2 = y + h/2
-    return x1,y1,x2,y2
 
-def local_zero(x1,y1,x2,y2):
-    if x1 < 0:
-        x2 = x1 + x2
-        x1 = 0
-    if y1 < 0:
-        y2 = y1 + y2
-        y1 = 0
-    return x1,y1,x2,y2
 
 def create_sample(w,h,gridw,gridh,inx):
     pic = np.zeros((h,w,3))
     rdxywh = get_rdxywh(w,h,gridw,gridh)
-    x1,y1,x2,y2 = local_zero(*to_xyxy(*rdxywh[:-2]))
+    x1,y1,x2,y2 = to_xyxy(*rdxywh[:-2])
     scax,scay = rdxywh[-2:]
-    pic[x1:x2,y1:y2,inx]=255
-    return pic,x1,y1,x2,y2,rdxywh
+    x1,y1,x2,y2 = local_size(x1,y1,x2,y2,w,h)
+    pic[y1:y2,x1:x2,inx]=255
+    xx,yy,ww,hh = to_xywh(x1,y1,x2,y2)
+    return pic,xx,yy,ww,hh
 
 
-
-f = open('pic.txt','a')
-
-if not os.path.isdir('pic'):
-    os.mkdir('pic')
 
 for i in range(20):
     color = rd.choice(['red','green','blue'])
@@ -70,10 +89,10 @@ for i in range(20):
     if color == 'green': inx = 1
     if color == 'blue': inx = 0
     picpath = 'pic/%05d.jpg'%i
-    pic,x1,y1,x2,y2,rdxywh = create_sample(w,h,gridw,gridh,inx)
+    pic,xx,yy,ww,hh = create_sample(w,h,gridw,gridh,inx)
     cv2.imwrite(picpath,pic)
-    print color,x1,y1,x2,y2,rdxywh
-
-s = GridLable(twth = (448,448), gridwh = (3,3))
-s.create_label(xywh = (50,40,3,3))
+    print '{} {} {} {} {} {}'.format(picpath,xx,yy,ww,hh,color)
+    s = GridLable(twth = (w,h), gridwh = (3,3))
+    print s.create_label((xx,yy,ww,hh))
+    
 
